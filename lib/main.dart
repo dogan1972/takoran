86b4ffaac3df0562f18100fecbe5eb8e10950558
example <email@example.com>
2022-01-01 23:51:36 +0300

    Harici apiler ayıklandı.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-//import 'package:getwidget/getwidget.dart';
-//import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() => runApp(MyApp());
 
